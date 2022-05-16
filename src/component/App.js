4881050a85d43f8cdb0f331e647c8a41501b3af1
Val Kimani <valkimani@Vals-MacBook-Pro.local>
2022-05-16 12:32:38 -0400

    Added a property to test/psuh values. The value 67 in this case breaks the calculator.

--- src/component/App.js
+++ src/component/App.js
@@ -16,6 +16,9 @@ export default class App extends React.Component {
   };
 
   render() {
+    if(this.state.next == "67"){
+      return new Error ("That value is wrong!");
+    }
     return (
       <div className="component-app">
         <Display value={this.state.next || this.state.total || "0"} />
